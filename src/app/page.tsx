328d5d2467024d68fd6ea503a44ab6f4898d1701
Yiwei Liu <liu02yiwei@gmail.com>
2024-11-17 22:47:11 +0000

    feat: basic structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <h1>Find events around you</h1>
-      <p>Browse more than 10,000 events around you</p>
+    <main className="flex flex-col items-center px-3 pt-36">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tighter">
+        Find events around you
+      </h1>
+      <p className="mb-12 mt-7 text-2xl opacity-75">
+        Browse more than{" "}
+        <span className="font-bold text-accent italic underline">
+          10,000 events
+        </span>{" "}
+        around you
+      </p>
 
-      <form>
-        <input placeholder="Search events in any city..." spellCheck={false} />
+      <form className="w-full sm:w-[580px]">
+        <input
+          placeholder="Search events in any city..."
+          spellCheck={false}
+          className="w-full h-16 rounded-lg px-6 outline-none bg-white/[7%] ring-accent/50 transition fucus:ring-2 focus:bg-white/20"
+        />
       </form>
 
-      <section>
+      <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular:</p>
-        <div>
+        <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seattle">Seattle</Link>
         </div>
