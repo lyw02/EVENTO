9cfd215174b4715357c66beb97c8b9a724a88302
Yiwei Liu <liu02yiwei@gmail.com>
2024-11-17 11:12:39 +0000

    feat: basic structure

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import Logo from "@/components/logo";
+import Link from "next/link";
+
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 md:px-9 ">
+      <Logo />
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
